22d11fbf8449cf4ed6f3263816702faf7879f80c
potharajutharun <potharajutharunrana@gmail.com>
2025-03-18 00:42:29 +0530

    added scripts

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // CORS Configuration
 const corsOptions = {
-  origin: 'http://localhost:3001', // Allow only requests from this domain
+  origin: process.env.CLIENT_URL, // Allow only requests from this domain
   methods: ['GET', 'POST','PUT','DELETE'], // Allow specific HTTP methods
   allowedHeaders: ['Content-Type'], // Allow specific headers
 };
